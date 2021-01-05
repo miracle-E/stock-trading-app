3ee771bae934802f72deac103d2692782ef7fb2c
miracle-E <miraclekalu1212@gmail.com>
2021-01-05 19:17:09 +0100

    Minor modifications

--- components/templates1/footer.vue
+++ components/templates1/footer.vue
@@ -1,5 +1,11 @@
 <template>
-    <footer class="w-full pin-b h-20 text-center p-4 bg-grey-darkest text-xl">
-           <p><span class="text-teal-lighter"> Vue Training &copy; 2019,</span> <span class="text-teal-light">Made by:</span> <span class="text-white hover:text-2xl hover: hover:text-teal"> BABA_TEZZ</span></p>
-    </footer>
+  <footer class="w-full pin-b h-20 text-center p-4 bg-grey-darkest text-xl">
+    <p>
+      <span class="text-teal-lighter"> Vue Training &copy; 2020,</span>
+      <span class="text-teal-light">Made by:</span>
+      <span class="text-white hover:text-2xl hover: hover:text-teal"
+        >Miracle</span
+      >
+    </p>
+  </footer>
 </template>
